68e82ada5da6a2ea2e7f94abc3be77f4cee3de06
Johan <rimkahan8@gmail.com>
2025-08-13 19:25:13 +1200

    2

--- quote_generator.py
+++ quote_generator.py
@@ -5,6 +5,19 @@
 import random
 import time
 
+# List of quotes with their authors
+QUOTES = [
+    {"text": "Be the change you wish to see in the world.", "author": "Mahatma Gandhi"},
+    {"text": "The only way to do great work is to love what you do.", "author": "Steve Jobs"},
+    {"text": "Life is what happens when you're busy making other plans.", "author": "John Lennon"},
+]
+
+# Configuration settings
+SETTINGS = {
+    "delay": 2,  # Seconds between quotes
+    "max_display": 5,  # Maximum quotes to display in one session
+}
+
 def main():
     """Main function to run the quote generator"""
     print("Welcome to the Random Quote Generator!")
