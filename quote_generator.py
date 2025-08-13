85cb0b6db8c0174366c8e796e1224866cf3e141e
Johan <rimkahan8@gmail.com>
2025-08-13 19:24:58 +1200

    1

---
+++ quote_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Random Quote Generator
+# A simple script to display random inspirational quotes
+
+import random
+import time
+
+def main():
+    """Main function to run the quote generator"""
+    print("Welcome to the Random Quote Generator!")
+    # More code will be added in future stages
+
+if __name__ == "__main__":
+    main()
